3c4ec2be67e7b72c718c8c84b8024a1c52024907
yang <1243604607@qq.com>
2020-04-22 16:57:57 +0800

    liu change 1 to 3

--- src/test1/Test.java
+++ src/test1/Test.java
@@ -3,7 +3,7 @@ package test1;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("branch2");
-		System.out.print("yang change branch1 to branch2");
+        System.out.println("branch3");
+        System.out.println("yang change branch1 to branch2 AND liu change branch1 to branch3");
     }
 }
