6879bc0947eadc0a483db6a5c0657fae75d7041a
rookiely1 <15556352210@163.com>
2020-04-22 18:01:26 +0800

    after change

--- src/test1/Test.java
+++ src/test1/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("branch3");
         System.out.println("yang change branch1 to branch2 AND liu change branch1 to branch3");
+		System.out.print("ly change");
     }
 }
